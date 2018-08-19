211dffdc5fb7e9da2c6d7a9ce20ff914d1a2cf6d
wzelichowski <wzelichowski@gmail.com>
2018-08-19 11:14:42 +0200

    finished js part and removed static numbers grom html

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-const fullDate = new Date();
-const hours = fullDate.getHours();
-const minutes = fullDate.getMinutes();
-const seconds = fullDate.getSeconds();
+function clock(){
+  const fullDate = new Date();
+  let hours = fullDate.getHours();
+  let minutes = fullDate.getMinutes();
+  let seconds = fullDate.getSeconds();
+
+  if (hours < 10) {
+    hours = '0' + hours;
+  }
+  if (minutes < 10) {
+    minutes = '0' + minutes;
+  }
+  if (seconds < 10) {
+    seconds = '0' + seconds;
+  }
+
+  document.getElementById('hour').innerHTML = hours; 
+  document.getElementById('minute').innerHTML = ': ' + minutes;
+  document.getElementById('second').innerHTML = ': ' + seconds;
+}
+setInterval(clock, 1000);
